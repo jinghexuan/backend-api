53b39e56dfbc4e8156f7cfee12badfdecb61e53b
jinghexuan <18754165579@163.com>
2026-05-14 13:41:38 +0800

    Update Book.java

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity;
 import java.time.LocalDateTime;
-
+import lombok.Data;
+import lombok.ToString;
+@Data
 public class Book {
-   private Long id;
+        private Long id;
         private String title;
         private String author;
         private String isbn;
@@ -10,78 +12,8 @@ public class Book {
         private Integer total;
         private Integer available;
         private LocalDateTime createTime;
-
-
-        public Book() {
-        }//无参构造方法，如果不写，会自动加一个
-
-        public Long getId() {
-            return id;
-        }//读取数据拿数据
-
-        public void setId(Long id) {
-            this.id = id;
-        }//修改数据加数据
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
-        public String getIsbn() {
-            return isbn;
-        }
-
-        public void setIsbn(String isbn) {
-            this.isbn = isbn;
-        }
-
-        public Integer getTotal() {
-            return total;
-        }
-
-        public void setTotal(Integer total) {
-            this.total = total;
-        }
-
-        public Integer getAvailable() {
-            return available;
-        }
-
-        public void setAvailable(Integer available) {
-            this.available = available;
-        }
-
-        public LocalDateTime getCreateTime() {
-            return createTime;
-        }
-
-        public void setCreateTime(LocalDateTime createTime) {
-            this.createTime = createTime;
-        }
-
-        // 重写toString，方便调试打印
-        @Override
-        public String toString() {
-            return "Book{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    ", author='" + author + '\'' +
-                    ", isbn='" + isbn + '\'' +
-                    ", total=" + total +
-                    ", available=" + available +
-                    ", createTime=" + createTime +
-                    '}';
-        }
 }
+      
+
+
+      
