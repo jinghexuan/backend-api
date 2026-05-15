5a3e967a4e9d39216f7da14fe127f448168e3525
jinghexuan <18754165579@163.com>
2026-05-15 19:36:46 +0800

    Update BorrowRecord.java

--- src/main/java/com/example/demo/entity/BorrowRecord.java
+++ src/main/java/com/example/demo/entity/BorrowRecord.java
@@ -1,4 +1,29 @@
-package com.example.demo.entity;
 
+  package com.example.demo.entity;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class BorrowRecord {
+
+   
+    private Integer id;
+
+    private Integer bookId;
+    private Integer userId;
+
+    private LocalDateTime borrowedAt;
+
+  
+    private LocalDateTime dueAt;
+
+    private LocalDateTime returnedAt;
+
+    private Integer status;
+
+    private Integer renewCount;
+
+    private LocalDateTime lastRenewedAt;
 }
