21766ad36173370e893202719b236b25ef790cca
Jinghexuan <18754165579@163.com>
2026-05-19 10:38:11 +0800

    mapper

--- src/main/java/com/example/demo/mapper/ReviewMapper.java
+++ src/main/java/com/example/demo/mapper/ReviewMapper.java
@@ -2,9 +2,13 @@ package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import java.math.BigDecimal;
 
 @Mapper
 public interface ReviewMapper {
     @Select("SELECT COUNT(*) FROM review")
     int countReview();
+
+    @Select("SELECT AVG(rating) FROM review WHERE book_id = #{bookId}")
+    BigDecimal avgRatingByBookId(Integer bookId);
 }
